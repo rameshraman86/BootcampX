d7b661c51fe256025c446cce6df223142dff3d63
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-24 13:22:14 -0400

    bootcamp queries 2

---
+++ 2_queries_joins/2_cohort_assignment_duration.sql
@@ -0,0 +1,8 @@
+SELECT
+  SUM(assignment_submissions.duration) AS total_duration
+FROM
+  assignment_submissions
+  JOIN students ON assignment_submissions.student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+where
+  cohorts.name = 'FEB12';
